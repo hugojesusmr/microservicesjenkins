8ee060e82b00570d297fd817e3ee9a2db0185a5b
hugojesusmr <hugojesusmr@gmail.com>
2022-10-03 20:18:15 +0000

    c

--- order-service/src/main/java/com/microserviceorder/orderservice/controller/OrderController.java
+++ order-service/src/main/java/com/microserviceorder/orderservice/controller/OrderController.java
@@ -1,13 +1,10 @@
 package com.microserviceorder.orderservice.controller;
 
-import java.util.List;
-
 import org.springframework.http.HttpStatus;
 import org.springframework.http.ResponseEntity;
 import org.springframework.web.bind.annotation.PostMapping;
 import org.springframework.web.bind.annotation.RequestBody;
 import org.springframework.web.bind.annotation.RequestMapping;
-import org.springframework.web.bind.annotation.ResponseStatus;
 import org.springframework.web.bind.annotation.RestController;
 import com.microserviceorder.orderservice.dto.OrderRequest;
 import com.microserviceorder.orderservice.entity.Order;
--- order-service/src/main/java/com/microserviceorder/orderservice/entity/Order.java
+++ order-service/src/main/java/com/microserviceorder/orderservice/entity/Order.java
@@ -1,6 +1,4 @@
 package com.microserviceorder.orderservice.entity;
-
-import java.util.ArrayList;
 import java.util.List;
 
 import javax.persistence.CascadeType;
