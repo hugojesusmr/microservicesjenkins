6609c7bc28081e468e00987462169922fb39ac0f
hugojesusmr <hugojesusmr@gmail.com>
2022-10-06 23:33:42 +0000

    c

--- product-service/src/main/java/com/microservice/productservice/ProductServiceApplication.java
+++ product-service/src/main/java/com/microservice/productservice/ProductServiceApplication.java
@@ -1,11 +1,14 @@
 package com.microservice.productservice;
 
 import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.EnableAutoConfiguration;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
+import org.springframework.boot.autoconfigure.mongo.MongoAutoConfiguration;
 import org.springframework.cloud.netflix.eureka.EnableEurekaClient;
 
 @SpringBootApplication
 @EnableEurekaClient
+@EnableAutoConfiguration(exclude={MongoAutoConfiguration.class})
 public class ProductServiceApplication {
 
 	public static void main(String[] args) {
