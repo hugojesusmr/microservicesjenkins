48c1246a901615c0ede2e2d5f133a56de827da44
hugojesusmr <hugojesusmr@gmail.com>
2022-10-07 18:41:03 +0000

    c

--- inventory-service/src/main/java/com/microservice/inventoryservice/InventoryServiceApplication.java
+++ inventory-service/src/main/java/com/microservice/inventoryservice/InventoryServiceApplication.java
@@ -2,11 +2,11 @@ package com.microservice.inventoryservice;
 
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
-import org.springframework.cloud.netflix.eureka.EnableEurekaClient; 
-/* import org.springframework.context.annotation.Bean;
+import org.springframework.cloud.netflix.eureka.EnableEurekaClient;
+import org.springframework.context.annotation.Bean;
 import org.springframework.boot.CommandLineRunner;
 import com.microservice.inventoryservice.model.Inventory;
-import com.microservice.inventoryservice.repository.InventoryRepository;  */
+import com.microservice.inventoryservice.repository.InventoryRepository;  
 
 @SpringBootApplication
 @EnableEurekaClient 
@@ -16,7 +16,7 @@ public class InventoryServiceApplication {
 		SpringApplication.run(InventoryServiceApplication.class, args);
 	}
 
-	/* @Bean
+	@Bean
 	public CommandLineRunner loadData(InventoryRepository inventoryRepository){
 		return args -> {
 			Inventory i = new Inventory();
@@ -28,5 +28,5 @@ public class InventoryServiceApplication {
 			inventoryRepository.save(i);
 			inventoryRepository.save(i2);
 		};
-	} */
+	}
 }
--- inventory-service/src/main/java/com/microservice/inventoryservice/model/Inventory.java
+++ inventory-service/src/main/java/com/microservice/inventoryservice/model/Inventory.java
@@ -12,7 +12,6 @@ import lombok.Data;
 import lombok.NoArgsConstructor;
 
 @Entity
-@Table(name="inventory")
 @AllArgsConstructor
 @NoArgsConstructor
 @Data
